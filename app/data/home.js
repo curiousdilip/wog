cb79b004edcd8f2605f4677d7f1167abe5b4b58a
Dilip Kumar <curiousdilip@gmail.com>
2023-10-19 16:24:18 +0530

    section roughly added

--- app/data/home.js
+++ app/data/home.js
@@ -35,3 +35,51 @@ export const guidanceBox = [
     link: "/second-coming",
   },
 ];
+
+export const aboutBox = [
+  {
+    src: "/icons/faith.svg",
+    title: "Bio",
+    link: "/bio",
+  },
+  {
+    src: "/icons/education.svg",
+    title: "Articles",
+    link: "/articles",
+  },
+  {
+    src: "/icons/jobs.svg",
+    title: "Messages",
+    link: "/message",
+  },
+  {
+    src: "/icons/marriage.svg",
+    title: "Events",
+    link: "/events",
+  }
+];
+
+
+export const contactBox = [
+  {
+    src: "/icons/faith.svg",
+    title: "Feedback",
+    link: "/feedback",
+  },
+  {
+    src: "/icons/education.svg",
+    title: "Counselling and Prayers",
+    link: "/counselling-and-prayers",
+  },
+  {
+    src: "/icons/jobs.svg",
+    title: "Career Guidance",
+    link: "/career-guidance",
+  },
+  {
+    src: "/icons/marriage.svg",
+    title: "Invite me to Speak",
+    link: "/invite-me",
+  }
+];
+
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Header from "./components/header";
-import { guidanceBox } from "./data/home";
+import { aboutBox, contactBox, guidanceBox } from "./data/home";
 import "./home.css";
 export default function Home() {
   return (
@@ -47,14 +47,80 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section>
-        <h3>Section 2</h3>
+
+      <section id="objectives">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-6 col-md-10 ">
+              <div className="section-heading text-center">
+                <h2>Objectives</h2>
+                <p>the objectives of this informal website are as follows:</p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <ul>
+                <li>To freely guide jobless youth for positive approaches to education, career, life and family, with wisdom from the Bible, especially the teaching of Lord Jesus Christ.</li>
+                <li>To freely counsel young professionals on authentic leadership, work-life balance, peaceful relationships and networking for extension of God's Kingdom through voluntary service in the Church and wider community.</li>
+                <li>To freely guide individuals to overcome personal crises by offering hope from the Bible and through prayers.</li>
+              </ul>
+              <p> Jesus said, 'Let your light shine before others, that they may see your good deeds and glorify your Father in heaven (Mathew 5:16).'' I welcome you to partner with me in this humble and meaningful endeavor, in your free time, in person or online from wherever you live.</p>
+              <p> With brotherly love,</p>
+              <p>A n a n d</p>
+
+            </div>
+          </div>
+        </div>
       </section>
-      <section>
-        <h4>Section 3</h4>
+      <section id="home-about">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-6 col-md-10 ">
+              <div className="section-heading text-center">
+                <h2>About</h2>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="box-container">
+                {aboutBox.map((item, index) => (
+                  <div className="box" key={index}>
+                    <img src={item.src} alt={item.title} />
+                    <h2>{item.title}</h2>
+                    <a href={item.link}>Learn More</a>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
-      <section>
-        <h5>Section 4</h5>
+
+      <section id="home-contact">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-6 col-md-10 ">
+              <div className="section-heading text-center">
+                <h2>Contact</h2>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="box-container">
+                {contactBox.map((item, index) => (
+                  <div className="box" key={index}>
+                    <img src={item.src} alt={item.title} />
+                    <h2>{item.title}</h2>
+                    <a href={item.link}>Learn More</a>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
