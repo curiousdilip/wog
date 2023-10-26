cdcb5696a58a529a90763f7f8c0521f1bf802ae1
Dilip Kumar Maurya <curiousdilip@gmail.com>
2023-10-26 05:04:05 +0000

    footer completed

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,7 +1,73 @@
+import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      <h1>Footer</h1>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <Link href="/" className="footer-logo">
+                <h3>
+                  WORDOF<span>GUIDANCE</span>
+                </h3>
+              </Link>
+
+              <div className="connect">
+                <div className="location list">
+                  <i className="bi bi-geo-alt"></i>{" "}
+                  <p>
+                    Investment Advisory Wing: B 915, Advant <br /> IT Park,
+                    Sector - 142,Noida, Gautam
+                    <br /> Buddha Nagar -201305
+                  </p>
+                </div>
+                <div className="phone list">
+                  <i className="bi bi-telephone"></i> <p>+91 0001254325</p>
+                </div>
+                <div className="email list">
+                  <i className="bi bi-envelope"></i>{" "}
+                  <p>getintouch@domain.com</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="row footer-nav">
+                <div className="col-md-6">
+                  <h4>Links</h4>
+                  <ul>
+                    <li>
+                      <a href="#">Education</a>
+                    </li>
+                    <li>
+                      <a href="#">Job</a>
+                    </li>
+                    <li>
+                      <a href="#">Marriage</a>
+                    </li>
+                    <li>
+                      <a href="#">Christian Home</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-md-6">
+                  <h4>We Offer</h4>
+                  <ul>
+                    <li>
+                      <a href="#">Counselling & Prayers</a>
+                    </li>
+                    <li>
+                      <a href="#">Career Guidance</a>
+                    </li>
+                    <li>
+                      <a href="#">Invite me</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Footer from "./components/footer";
 import Header from "./components/header";
 import { aboutBox, contactBox, guidanceBox } from "./data/home";
 import "./home.css";
-Image
+Image;
 export default function Home() {
   return (
     <>
@@ -25,29 +26,64 @@ export default function Home() {
           </div>
         </div>
       </section>
+
       <section id="hero-details">
+        {" "}
+        <div className="hero-img"></div>
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <div className="content">
-                <Image
+                {/* <Image
                   src="/hero-img.jpeg"
                   width={800}
                   height={500}
                   alt="Hero Image"
                   className="my-3 img-fluid w-100"
-                />
-                <p>He is light because divine glory was evident in Him as the life giver. As He is light, demons (powers of darkness) flee away from His Presence! Jesus is light because He delivers us from darkness (sinful living under the control of Satan) and leads us to a life of holiness. He is light because His words guide us on our pilgrimage to heaven. Christ is our model as He is the Way to holiness, the embodiment of Truth that liberates and blesses us with a life that is abundant in meaning and purpose. Jesus is calling us to be His disciples so that we can also live as He lived, with holiness, humility, honesty and love. The content provided in this website will also equip you for every good work for His glory.
+                /> */}
+                <p>
+                  He is light because divine glory was evident in Him as the
+                  life giver. As He is light, demons (powers of darkness) flee
+                  away from His Presence! Jesus is light because He delivers us
+                  from darkness (sinful living under the control of Satan) and
+                  leads us to a life of holiness. He is light because His words
+                  guide us on our pilgrimage to heaven. Christ is our model as
+                  He is the Way to holiness, the embodiment of Truth that
+                  liberates and blesses us with a life that is abundant in
+                  meaning and purpose. Jesus is calling us to be His disciples
+                  so that we can also live as He lived, with holiness, humility,
+                  honesty and love. The content provided in this website will
+                  also equip you for every good work for His glory.
                 </p>
-                <p>Furthermore the objectives of this informal website are as follows:
-
+                <p>
+                  Furthermore the objectives of this informal website are as
+                  follows:
                 </p>
                 <ul>
-                  <li>To freely guide jobless youth for positive approaches to education, career, life and family, with wisdom from the Bible, especially the teaching of Lord Jesus Christ.</li>
-                  <li>To freely counsel young professionals on authentic leadership, work-life balance, peaceful relationships and networking for extension of God's Kingdom through voluntary service in the Church and wider community.</li>
-                  <li>To freely guide individuals to overcome personal crises by offering hope from the Bible and through prayers.</li>
+                  <li>
+                    To freely guide jobless youth for positive approaches to
+                    education, career, life and family, with wisdom from the
+                    Bible, especially the teaching of Lord Jesus Christ.
+                  </li>
+                  <li>
+                    To freely counsel young professionals on authentic
+                    leadership, work-life balance, peaceful relationships and
+                    networking for extension of God's Kingdom through voluntary
+                    service in the Church and wider community.
+                  </li>
+                  <li>
+                    To freely guide individuals to overcome personal crises by
+                    offering hope from the Bible and through prayers.
+                  </li>
                 </ul>
-                <p> Jesus said, 'Let your light shine before others, that they may see your good deeds and glorify your Father in heaven (Mathew 5:16).'' I welcome you to partner with me in this humble and meaningful endeavor, in your free time, in person or online from wherever you live.</p>
+                <p>
+                  {" "}
+                  Jesus said, 'Let your light shine before others, that they may
+                  see your good deeds and glorify your Father in heaven (Mathew
+                  5:16).'' I welcome you to partner with me in this humble and
+                  meaningful endeavor, in your free time, in person or online
+                  from wherever you live.
+                </p>
                 <p> With brotherly love,</p>
                 <p>A n a n d</p>
               </div>
@@ -129,6 +165,7 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <Footer />
     </>
   );
 }
