23bcd5bc05df6f9a9b9f14e18d23178cbd3e8839
Dilip Kumar <curiousdilip@gmail.com>
2023-10-28 16:25:39 +0530

    job page added

---
+++ app/job/page.js
@@ -0,0 +1,152 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+import "./job.css";
+export default function Job() {
+    return (
+        <>
+            <Header />
+            <section id="job">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="content">
+                                <div className="topic">
+                                    <h3>For Jobseekers</h3>
+                                </div>
+                                <div className="topic">
+                                    <h4>
+                                        Do you see someone skilled in their work? They will serve
+                                        before kings; they will not serve before officials of low
+                                        rank. (Proverbs 22:29)
+                                    </h4>
+                                    <p>
+                                        You may check the following popular websites frequently, or
+                                        subscribe to some of them to receive updates daily or
+                                        weekly. You can also upload your resume, so that potential
+                                        employers can contact you. I have no connection with any of
+                                        these popular websites, but I know them to be highly visited
+                                        by countless jobseekers, every hour.
+                                    </p>
+                                </div>
+                                <div className="topic">
+                                    <h4>
+                                        Do you see someone skilled in their work? They will serve
+                                        before kings; they will not serve before officials of low
+                                        rank. (Proverbs 22:29)
+                                    </h4>
+                                    You may check the following popular websites frequently, or
+                                    subscribe to some of them to receive updates daily or weekly.
+                                    You can also upload your resume, so that potential employers
+                                    can contact you. I have no connection with any of these
+                                    popular websites, but I know them to be highly visited by
+                                    countless jobseekers, every hour.
+                                </div>
+                                <div className="topic">
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <p>
+                                                <a href="http://www.ncs.gov.in">www.ncs.gov.in</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.ngobox.org">www.ngobox.org</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.devnetjobsindia.org">
+                                                    www.devnetjobsindia.org
+                                                </a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.ngojobsindia.in">
+                                                    www.ngojobsindia.in
+                                                </a>
+                                                &nbsp;
+                                            </p>
+                                            <p>
+                                                <a href="http://www.devnetjobs.org">
+                                                    www.devnetjobs.org
+                                                </a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.ngosindia.com">www.ngosindia.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.indianngos.com">
+                                                    www.indianngos.com
+                                                </a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.careerjet.co.in">
+                                                    www.careerjet.co.in
+                                                </a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.naukri.com">www.naukri.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.monster.com">www.monster.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.careerbuilder.com">
+                                                    www.careerbuilder.com
+                                                </a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.unjobs.com">www.unjobs.com</a>
+                                            </p>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <p>
+                                                <a href="http://www.indeed.com">www.indeed.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.nsdcindia.org">www.nsdcindia.org</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.simplyhired.com">
+                                                    www.simplyhired.com
+                                                </a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.glassdoor.com">www.glassdoor.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.jobs.aol.com">www.jobs.aol.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.snagajobs.com">www.snagajobs.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.theladders.com">
+                                                    www.theladders.com
+                                                </a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.careerjet.com">www.careerjet.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.usajobs.com">www.usajobs.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.job.com">www.job.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.dice.com">www.dice.com</a>
+                                            </p>
+                                            <p>
+                                                <a href="http://www.beyond.com">www.beyond.com</a>
+                                            </p>
+                                        </div>
+                                        <p>If you require counselling for career
+                                            <a href="https://wordofguidance.in/career-guidance/">click here</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </>
+    );
+}
