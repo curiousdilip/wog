296e09513982cf323052110d4efb3cbc7be63e51
Dilip Kumar Maurya <curiousdilip@gmail.com>
2023-11-03 19:10:44 +0000

    Create messages.css and page.js

---
+++ app/messages/page.js
@@ -0,0 +1,103 @@
+import Breadcrumbs from "../components/breadcrumbs";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import "./messages.css";
+export default function Messages() {
+  return (
+    <>
+      <Header />
+      <Breadcrumbs pageName="Messages" />
+      <section id="messages">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="content">
+                <div className="topic">
+                  <h2>Video Messages</h2>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#">
+                      <i className="bi bi-camera-video"> </i> Family – God’s
+                      Gift
+                    </a>
+                  </p>
+                </div>
+                <div className="topic">
+                    <h2>
+                        Audio Messages
+                    </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
