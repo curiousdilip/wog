53e228e877b5a55bfe6bbafbec8c08ffdce9dea4
Dilip Kumar <curiousdilip@gmail.com>
2023-11-04 01:37:27 +0530

    contact pages initiated

---
+++ app/contact/counselling-and-prayers/page.js
@@ -0,0 +1,29 @@
+import Breadcrumbs from "@/app/components/breadcrumbs";
+import Footer from "@/app/components/footer";
+import Header from "@/app/components/header";
+import "./cap.css"
+export default function Feedback() {
+    return (
+        <>
+            <Header />
+            <Breadcrumbs pageName="Counselling And Prayers
+ " />
+            <section id="cap">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="content">
+                                <div className="topic">
+                                    <h3>You can contact A n a n d for counselling and prayers. Your messages below will NOT be posted
+                                        anywhere online on this website or anywhere on the internet. Thanks.
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </>
+    )
+}
