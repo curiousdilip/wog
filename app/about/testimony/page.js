41e17d559d67b5f0258ed4176ab0b2e99717d30e
Dilip Kumar Maurya <curiousdilip@gmail.com>
2023-11-07 06:27:28 +0000

    Testimony page added

---
+++ app/about/testimony/page.js
@@ -0,0 +1,122 @@
+import Breadcrumbs from "@/app/components/breadcrumbs";
+import Footer from "@/app/components/footer";
+import Header from "@/app/components/header";
+import "./testimony.css";
+export default function Testimony() {
+  return (
+    <>
+      <Header />
+      <Breadcrumbs pageName="Testimony" />
+      <section id="testimony">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="content">
+                <div className="topic">
+                  <p>
+                    I was born in Chennai (then called Madras), in South India
+                    in December 1969. My father was a Pastor of Arcot Lutheran
+                    Church (ALC Chennai and nearby districts) and my mother
+                    worked as a teacher in a government-run school,
+                    Tiruvannamalai (near Chennai). I have an elder brother and
+                    an elder sister who are leading their own lives along with
+                    their families. My father served ALC for nearly four decades
+                    and was well-known for his walking the extra mile for people
+                    in need. He had his ministry internship at Peace Evangelical
+                    Lutheran Church, Philadelphia, US (1970-71) and later taught
+                    Christian ministry and Pastoralia at the Tamil Nadu
+                    Theological Seminary, Madurai (1982-85). Alongside his
+                    pastoral calling, with deep personal interest, he ministered
+                    to the gypsies (Narikuravars/Vagiris) in Tiruvannamalai and
+                    nearby districts for over three decades.{" "}
+                  </p>
+                  <p>
+                    My mother came from the mainline Church of South India
+                    background, from Bangalore, but knew God just in her mind
+                    and neither led a life of regular prayer nor was hospitable
+                    to the congregation. Mother suffered from complex emotional
+                    breakdowns, hated my father and us, children. She was
+                    ruthlessly judgmental, and used to humiliate us to any level
+                    in order to ensure that we were no “problem kids!” At
+                    school, though teachers were generally talented, the
+                    educational system was arcane, with a third-class syllabus,
+                    that neither inspired you nor was promising to take you
+                    anywhere up in life.
+                  </p>
+                  <p>
+                    With this backdrop, my childhood prayers were not for my own
+                    development or my future, but for peace and quietness at
+                    home. In April 1980, as a school boy, I attended a massive
+                    open-air evangelistic meeting in Cuddalore, South India held
+                    by renowned non-denominational healing preacher, Brother
+                    Dhinakaran. I deeply felt that I needed a Savior for my
+                    troubled home and a redeemer of my soul. I confessed my sins
+                    to the Lord Jesus and surrendered my life. I got a
+                    reassuring feeling that God heard my prayers. I felt a
+                    newness in my heart and a surge of courage to live
+                    differently than other boys in School. With the
+                    encouragement of my father, I began reading the Bible and
+                    Christian books and magazines. I came to believe that God
+                    saved me by His grace and through creating in me a living
+                    faith. I came to know that I am saved not BY works but FOR
+                    works of faith as His witness in a fallen world. As per
+                    church’s teaching, I began confessing with meaning the
+                    Apostolic creed of faith, every Sunday. An elder friend of
+                    my father – Brother Darling Samuel took personal interest in
+                    me and led me to a deeper understanding of the Bible, by
+                    hours of conversations. Another friend of my father (Brother
+                    Augustine Joseph), counseled me on how to deal with
+                    temptations and youthful sins and prayed for me that I will
+                    ever have a closer walk with the Lord, in holiness.
+                  </p>
+                  <p>
+                    As college life began, I continued my regular study of the
+                    Bible and prayers in solitude of my hostel room. I found God
+                    strengthening me through His promises in the Bible. I found
+                    the love of the Heavenly Father, who called me from darkness
+                    to light. I found God’s grace in Christ Jesus who has been
+                    interceding for me, being seated at the right hand of the
+                    heavenly Father. I found the joy of Salvation through the
+                    sweet communion of the Holy Spirit, by attending spiritual
+                    conventions as and when possible. I began to share about my
+                    faith life to fellow students at the chapel and at student
+                    bible study groups, small fellowships, and later in
+                    churches. I began writing articles in local church
+                    bulletins.
+                  </p>
+                  <p>
+                    In Oct 1999, God led me to marry a God-fearing young woman
+                    Sharmilee, from Andhra Pradesh. She was greatly guided by
+                    her mother in knowing the Lord intimately and to worship Him
+                    by her personal devotions and prayers. Sharmilee has been
+                    greatly helpful in my humble ministry engagements, for over
+                    two decades, while I have been in secular jobs in the
+                    development sector and of late, had served in a regional
+                    institute of the United Nations.
+                  </p>
+                  <p>
+                    I am sharing this testimony to encourage people who question
+                    God’s ways in their lives. Look beyond your depressing
+                    circumstances for a better tomorrow, as the Good Shepherd
+                    can lead you from shame to glory. Irrespective of your
+                    family or religious background if you surrender your life to
+                    the Lord Jesus Christ, by confessing your sins and turning
+                    away from them, you can see His faithfulness in forgiving
+                    you and granting His gift of Salvation, where life will
+                    assume fuller meaning. Your journey of faith will be
+                    purpose-driven with the enabling grace of God, liberating
+                    truth of the Bible and the leading of the Holy spirit. May
+                    God bless you to taste and see in life, that He is good!
+                  </p>
+
+                  <a href="/about/bio">Back to Bio</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
