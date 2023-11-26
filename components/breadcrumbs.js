c6cf54cc9aca02eea6d27de3376276a7a085e431
Dilip Kumar <dilipkumar@Dilips-Mac.local>
2023-11-26 22:41:35 +0530

    added details in footer

--- components/breadcrumbs.js
+++ components/breadcrumbs.js
@@ -6,15 +6,6 @@ const Breadcrumbs = ({ pageName }) => {
     <>
       <div className="breadcrumbs">
         <div className={pageName}>
-          {/* <Image
-                        className='bg-image'
-                        src="/about-breadcrumb.jpg"
-                        alt="About Breadcrumbs photos"
-                        layout='fill'
-                        objectFit='cover'
-                        objectPosition='center top'
-                        style={{ zIndex: "0" }}
-                    /> */}
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
--- components/footer.js
+++ components/footer.js
@@ -14,52 +14,64 @@ export default function Footer() {
 
               <div className="connect">
                 <div className="location list">
-                  <i className="bi bi-geo-alt"></i>{" "}
+                  <i className="bi bi-geo-alt"></i>
                   <p>
-                    Investment Advisory Wing: B 915, Advant <br /> IT Park,
-                    Sector - 142,Noida, Gautam
-                    <br /> Buddha Nagar -201305
+                    G 32, Block C1,
+                    <br />
+                    BK Dutt Colony, New Delhi,
+                    <br />Delhi 110003
                   </p>
                 </div>
                 <div className="phone list">
-                  <i className="bi bi-telephone"></i> <p>+91 0001254325</p>
+                  <i className="bi bi-telephone"></i> <p>+91 98688 83434</p>
                 </div>
                 <div className="email list">
-                  <i className="bi bi-envelope"></i>{" "}
-                  <p>getintouch@domain.com</p>
+                  <i className="bi bi-envelope"></i>
+                  <p>wordofguidance@gmail.com</p>
                 </div>
               </div>
             </div>
             <div className="col-md-6">
               <div className="row footer-nav">
                 <div className="col-md-6">
-                  <h4>Links</h4>
+                  <h4>Guidance</h4>
                   <ul>
                     <li>
-                      <a href="#">Education</a>
+                      <Link href="/faith">Faith</Link>
                     </li>
                     <li>
-                      <a href="#">Job</a>
+                      <Link href="/education">Education</Link>
+
                     </li>
                     <li>
-                      <a href="#">Marriage</a>
+                      <Link href="/job">Job</Link>
                     </li>
                     <li>
-                      <a href="#">Christian Home</a>
+                      <Link href="/marriage">Marriage</Link>
                     </li>
+                    <li>
+                      <Link href="/christian-home">Christian Home</Link>
+                    </li>
+                    <li>
+                      <Link href="/serving-god">Serving God</Link>
+                    </li>
+                    <li>
+                      <Link href="/second-coming">Second Coming</Link>
+                    </li>
+
                   </ul>
                 </div>
                 <div className="col-md-6">
-                  <h4>We Offer</h4>
+                  <h4>About</h4>
                   <ul>
                     <li>
-                      <a href="#">Counselling & Prayers</a>
+                      <Link href="/about/bio">Bio</Link>
                     </li>
                     <li>
-                      <a href="#">Career Guidance</a>
+                      <Link href="/about/messages">Messages</Link>
                     </li>
                     <li>
-                      <a href="#">Invite me</a>
+                      <Link href="/about/events">Events</Link>
                     </li>
                   </ul>
                 </div>
